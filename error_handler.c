b78126e27f30685b4939f007177e285e01ef1ca0
ermi1999 <ermiyasabiye1999@gmail.com>
2023-09-19 14:59:53 -0700

    error handler

--- error_handler.c
+++ error_handler.c
@@ -28,3 +28,16 @@ void command_not_found_error_handler(char *program_name, char *arg)
  * @program_name: The name of the program
  * @arg: The illegal number argument
  */
+void exit_error_handler(char *program_name, char *arg)
+{
+	char error_message[256];
+​
+	string_copy(error_message, program_name);
+	string_concat(error_message, ": 1: exit: Illegal number: ");
+	string_concat(error_message, arg);
+	string_concat(error_message, "\n");
+​
+	write(STDERR_FILENO, error_message, string_length(error_message));
+​
+	exit(2);
+}
